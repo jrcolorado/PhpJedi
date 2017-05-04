87a4df3e77b1e0db47d79a7ccffd206fc3b092d1
joaoromario <joaoromario@joaoromario-IUC8884>
2017-05-04 17:01:52 -0300

    Commit pastas de aulas 04/05/2017

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        echo "alterado"
-        ?>
-    </body>
-</html>
+<?php
+
+require 'Classes.php';
+
+$luz = new Luz();
+$c = new LuzOnCommand($luz);
+callCommand($c);
+echo "Luz: ".$luz->getStatus();
