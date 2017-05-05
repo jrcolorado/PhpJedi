384cbb54ab947a5eb60742bb9396766ecf7dab3b
joaoromario <joaoromario@joaoromario-IUC8884>
2017-05-04 21:20:46 -0300

    Commit pastas de aulas 04/05/2017

--- index.php
+++ index.php
@@ -1,8 +1,59 @@
+<a href="Home.html">Aqui um link para a pagina Home</a>
+
 <?php
 
-require 'Classes.php';
+interface VideoServiceInterface{
+    public function getEMBED();
+}
 
-$luz = new Luz();
-$c = new LuzOnCommand($luz);
-callCommand($c);
-echo "Luz: ".$luz->getStatus();
+class Youtube implements VideoServiceInterface{
+    private $url;
+    public function __construct($url) {
+        $this->url=$url;
+    }
+    public function getEMBED(){
+        return '<iframe>'.$this->url.'</iframe>';        
+    }
+}
+
+
+class Vimeo implements VideoServiceInterface{
+    private $url;
+    public function __construct($url) {
+        $this->url=$url;
+    }
+    public function getEMBED(){
+        return '<video>'.$this->url.'</video>';        
+    }
+}
+
+
+class Wistia implements VideoServiceInterface{
+    private $url;
+    public function __construct($url) {
+        $this->url=$url;
+    }
+    public function getEMBED(){
+        return '<coisa>'.$this->url.'</coisa>';        
+    }
+}
+class Aula{
+    
+    private $video;
+    
+    public function __construct(VideoServiceInterface $video) {
+        $this->video =$video;
+        }
+    public function getVideoHtml(){
+        return $this->video->getEMBED();
+    }
+        
+    }
+   /*
+    $video = new Youtube('https://youtu.be/1HaUtdwUprE');
+    $aula = new Aula($video);
+    
+    echo "video : ".$aula->getVideoHtml();
+    * */
+   
+   
